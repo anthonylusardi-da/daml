3e584cf846c908616c25252c76716fcbfe162c0a
Robin Krom <robin@digitalasset.com>
2020-03-25 12:41:08 +0100

    language:daml-ledger: tests for streaming methods
    This adds tests mocking a websocket server to test the
    `streamFetchByKey` and `streamQuery` methods of the ledger class.
    
    BEGIN_CHANGELOG
    END_CHANGELOG

---
+++ language-support/ts/daml-ledger/__mocks__/ws.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2020 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export { WebSocket as default } from "mock-socket";
+
---
+++ language-support/ts/daml-ledger/index.test.ts
@@ -0,0 +1,243 @@
+// Copyright (c) 2020 The DAML Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Template, ContractId, Choice } from "@daml/types";
+import WS from "jest-websocket-mock";
+import Ledger from "./index";
+import { Event, CreateEvent } from "./index";
+import * as jtv from "@mojotech/json-type-validation";
+
+type Foo = {};
+
+const Foo: Template<Foo, undefined, "foo-id"> = {
+  templateId: "foo-id",
+  keyDecoder: () => jtv.constant(undefined),
+  decoder: () => jtv.object({}),
+  Archive: ({} as unknown) as Choice<Foo, {}, {}, undefined>,
+};
+
+const fooEvent = (coid: number): Event<Foo, undefined, "foo-id"> => {
+  return { created: fooCreateEvent(coid) };
+};
+
+const fooCreateEvent = (
+  coid: number
+): CreateEvent<Foo, undefined, "foo-id"> => {
+  return {
+    templateId: "foo-id",
+    contractId: coid.toString() as ContractId<object>,
+    signatories: [],
+    observers: [],
+    agreementText: "fooAgreement",
+    key: undefined,
+    payload: {},
+  };
+};
+
+const fooArchiveEvent = (coid: number): Event<Foo, undefined, "foo-id"> => {
+  return {
+    archived: {
+      templateId: "foo-id",
+      contractId: coid.toString() as ContractId<object>,
+    },
+  };
+};
+
+const mockOptions = {
+  token: "dummyToken",
+  httpBaseUrl: "http://localhost:5000/",
+  wsBaseUrl: "ws://localhost:4000/",
+};
+
+afterEach(() => {
+  WS.clean();
+});
+
+describe("streamQuery", () => {
+  // we only check that nothing crashes.
+  test("receive unknown message", async () => {
+    const server = new WS("ws://localhost:4000/v1/stream/query", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamQuery(Foo);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (evs) => console.log(evs));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send("mickey mouse");
+  });
+
+  // we only check that nothing crashes.
+  test("receive warnings", async () => {
+    const server = new WS("ws://localhost:4000/v1/stream/query", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamQuery(Foo);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (evs) => console.log(evs));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ warnings: ["oh oh"] });
+  });
+
+  // we only check that nothing crashes.
+  test("receive errors", async () => {
+    const server = new WS("ws://localhost:4000/v1/stream/query", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamQuery(Foo);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (evs) => console.log(evs));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ errors: ["not good!"] });
+  });
+
+  test("receive empty events", async () => {
+    var receivedEvents: object[] = [];
+    const server = new WS("ws://localhost:4000/v1/stream/query", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamQuery(Foo);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (evs) => (receivedEvents = [...evs]));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [] });
+    expect(receivedEvents).toEqual([]);
+  });
+
+  test("receive one event", async () => {
+    var receivedEvents: object[] = [];
+    const server = new WS("ws://localhost:4000/v1/stream/query", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamQuery(Foo);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (evs) => (receivedEvents = [...evs]));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [fooEvent(1)] });
+    expect(receivedEvents).toEqual([fooCreateEvent(1)]);
+  });
+
+  test("receive several events", async () => {
+    var receivedEvents: object[] = [];
+    const server = new WS("ws://localhost:4000/v1/stream/query", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamQuery(Foo);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (evs) => (receivedEvents = [...evs]));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [1, 2, 3].map(fooEvent) });
+    expect(receivedEvents).toEqual([1, 2, 3].map(fooCreateEvent));
+  });
+
+  test("drop matching created and archived events", async () => {
+    var receivedEvents: object[] = [];
+    const server = new WS("ws://localhost:4000/v1/stream/query", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamQuery(Foo);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (evs) => (receivedEvents = [...evs]));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [fooEvent(1), fooEvent(2), fooArchiveEvent(1)] });
+    expect(receivedEvents).toEqual([fooCreateEvent(2)]);
+  });
+});
+
+describe("streamFetchByKey", () => {
+  test("receive no event", async () => {
+    var receivedEvent: object | null = null;
+    const server = new WS("ws://localhost:4000/v1/stream/fetch", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamFetchByKey(Foo, undefined);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (ev) => (receivedEvent = ev));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (ev) => console.log(ev));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [] });
+    expect(receivedEvent).toEqual(null);
+  });
+
+  test("receive one event", async () => {
+    var receivedEvent: object | null = null;
+    const server = new WS("ws://localhost:4000/v1/stream/fetch", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamFetchByKey(Foo, undefined);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (ev) => (receivedEvent = ev));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (ev) => console.log(ev));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [fooEvent(1)] });
+    expect(receivedEvent).toEqual(fooCreateEvent(1));
+  });
+
+  test("receive several events", async () => {
+    var receivedEvent: object | null = null;
+    const server = new WS("ws://localhost:4000/v1/stream/fetch", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamFetchByKey(Foo, undefined);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (ev) => (receivedEvent = ev));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (ev) => console.log(ev));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [fooEvent(1), fooEvent(2), fooEvent(3)] });
+    expect(receivedEvent).toEqual(fooCreateEvent(3));
+  });
+
+  test("drop matching created and archived events", async () => {
+    var receivedEvent: object | null = null;
+    const server = new WS("ws://localhost:4000/v1/stream/fetch", {
+      jsonProtocol: true,
+    });
+    const ledger = new Ledger(mockOptions);
+    const stream = ledger.streamFetchByKey(Foo, undefined);
+    await server.connected;
+    console.log("connected");
+    stream.on("change", (ev) => (receivedEvent = ev));
+    stream.on("close", (ev) => console.log(ev));
+    stream.off("change", (evs) => console.log(evs));
+    stream.off("close", (ev) => console.log(ev));
+    server.send({ events: [fooEvent(1), fooArchiveEvent(1)] });
+    expect(receivedEvent).toEqual(null);
+  });
+});
+
