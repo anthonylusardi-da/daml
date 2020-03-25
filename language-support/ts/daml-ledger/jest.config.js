1db034c1ac96fa8b8104cba65766092917c9f7e9
Robin Krom <robin@digitalasset.com>
2020-03-25 19:47:50 +0100

    remove unnecessary module name mapping

--- language-support/ts/daml-ledger/jest.config.js
+++ language-support/ts/daml-ledger/jest.config.js
@@ -3,26 +3,8 @@
 
 const moduleNameMapper = {
   '^@daml/types$': '../daml-types',
-  '^@daml/ledger$': '../daml-ledger',
-  // $1 used for @daml/react/ledgerStore
-  '^@daml/react(.*)$': '../daml-react$1'
+  '^@daml/ledger$': '../daml-ledger'
 };
-
-// Bazel workaround for:
-//
-//   Invalid hook call. Hooks can only be called inside of the body of a function component.
-//
-// By default the test-case loads `react/cjs/react.development.js` and
-// fails with the above error. A similar issue with `ts_devserver` suggests
-// that `rules_nodejs` bundles conflicting versions of react in some of the
-// artifacts. See https://github.com/bazelbuild/rules_nodejs/issues/1383 .
-//
-// We do not want to use this workaround when invoking the test with
-// `yarn test`. We us the existence of the environment variable
-// `TEST_WORKSPACE` as a proxy for whether the test was invoked from bazel.
-if (process.env.TEST_WORKSPACE !== undefined) {
-  moduleNameMapper['^react$'] = '<rootDir>/../../../node_modules/react/umd/react.development.js';
-}
 
 module.exports = {
   testEnvironment: "node",
